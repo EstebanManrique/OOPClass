0503c60d71325981c48112a89e6e0ff7bca995c3
EstebanManrique <a01027077@itesm.mx>
2019-01-19 12:05:12 -0600

    Actividades clase 18 de enero

---
+++ Actividades/A-1.1.-VARIOS/Person.java
@@ -0,0 +1,17 @@
+public class Person
+{
+    //attributes
+    private int age;
+    String name;
+    char gender;
+
+    //methods
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+}
---
+++ Actividades/A-1.1.-VARIOS/Tests.java
@@ -0,0 +1,9 @@
+public class Tests
+{
+    public static void main(String []args)
+    {
+        Person ariel=new Person();
+        ariel.setName("ariel");
+        System.out.println(ariel.getName());
+    }
+}
